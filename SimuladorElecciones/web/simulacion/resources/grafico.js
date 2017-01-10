ce75e5e7f1bdddd3f3bb60d5f23a394485b5b593
Zalez95 <dani-gonz@outlook.com>
2017-01-10 23:39:53 +0100

    graficos corregidos

--- SimuladorElecciones/web/simulacion/resources/grafico.js
+++ SimuladorElecciones/web/simulacion/resources/grafico.js
@@ -1,66 +1,31 @@
-/** DATOS **/
-// Dimensiones del grafico
-var margen          = { superior: 75, derecho: 50, inferior: 75, izquierdo: 50 },
-    ancho           = 650,
-    alto            = 650,
-    centro          = {x: ancho/2, y: alto},
-    radioExterior   = Math.min(ancho, alto) / 2,
-    radioInterior   = radioExterior - 50;
-
-
-/** FUNCIONES **/
 /*
- * Dibuja el grafico de sectores con los escaños de cada candidatura en cada una
- * de las circunscripciones dadas
+ * Dibuja el grafico de sectores con los escaños de cada candidatura en la
+ * circunscripcion dada
  */
-function dibujaGrafico(circunscripciones, candidaturas, votosCircunscripciones, propMinRepresentacion) {
-
-    for (var i in circunscripciones) {
-	var datos = calculaEscaños(circunscripciones[i], candidaturas, votosCircunscripciones[i], propMinRepresentacion);
-        if (datos === null) { continue; }
-
-	var svg = d3.select(".charts").append("svg")
-            .attr("width", ancho)
-            .attr("height", alto);
-	var groupArcos = svg.append("g")
-            .attr("transform", "translate(" + ancho/2 + ", " + alto/2 + ")");
-
-	ultimoAngulo = -Math.PI/2;
-	for (var j in datos) {
-            var anguloFinal = ultimoAngulo + datos[j].numeroEscaños * Math.PI / circunscripciones[i].numeroRepresentantes;
-
-            var arco = d3.svg.arc()
-                .outerRadius(radioExterior)
-                .innerRadius(radioInterior)
-                .startAngle(ultimoAngulo)
-                .endAngle(anguloFinal);
-
-            groupArcos.append("path")
-                .style("fill", datos[j].candidatura.color)
-                .attr("d", arco);
-
-            ultimoAngulo = anguloFinal;
-	}
-    }
-    
-}
-
 function dibujaGraficoPequeño(circunscripcion, candidaturas, votosCircunscripcion, propMinRepresentacion) {
     
     var datos = calculaEscaños(circunscripcion, candidaturas, votosCircunscripcion, propMinRepresentacion);
     
+    // Dibujamos el gráfico pequeño
     var contenedor = document.getElementById("charts1");
     while(contenedor.hasChildNodes()){
         contenedor.removeChild(contenedor.firstChild);
     }
     
-    var svg = d3.select("#charts1").append("svg")
-        .attr("width", ancho/2)
-        .attr("height", alto/2);
-    var groupArcos = svg.append("g")
-        .attr("transform", "translate(" + ancho/4 + ", " + alto/4 + ")");
+    var divPequeño = d3.select("#charts1");
+    var dimensionesDivPequeño = divPequeño.node().getBoundingClientRect(),
+        centro = {x: dimensionesDivPequeño.width/2, y: dimensionesDivPequeño.width/2},
+        radioExterior = dimensionesDivPequeño.width / 2,
+        radioInterior = radioExterior - 50;
 
-    ultimoAngulo = -Math.PI/2;
+    var svg = divPequeño.append("svg")
+        .attr("width", dimensionesDivPequeño.width)
+        .attr("height", dimensionesDivPequeño.width/2);
+
+    var groupArcos = svg.append("g")
+        .attr("transform", "translate(" + centro.x + ", " + centro.y + ")");
+
+    var ultimoAngulo = -Math.PI/2;
     for (var j in datos) {
         var anguloFinal = ultimoAngulo + datos[j].numeroEscaños * Math.PI / circunscripcion.numeroRepresentantes;
 
@@ -78,39 +43,59 @@ function dibujaGraficoPequeño(circunscripcion, candidaturas, votosCircunscripci
     }
 }
 
+/*
+ * Dibuja el grafico de sectores con los escaños de cada candidatura en cada una
+ * de las circunscripciones dadas
+ */
 function dibujaGraficoGrande(indice, circunscripciones, candidaturas, votosCircunscripciones, propMinRepresentacion) {
+
+    var numRepresentantes = 0;
+    for(var i = 0; i <= indice; i++){
+        numRepresentantes += circunscripciones[i].numeroRepresentantes;
+    }
     
+    var datos = new Array(candidaturas.length);
+    for (var i = 0; i < candidaturas.length; i++) {
+        datos[i] = { candidatura: candidaturas[i], numeroEscaños: 0 };
+    }
+    
+    for(var i = 0 ; i <= indice; i++){
+        // Calculamos los escaños en la circunscripcion actual
+        var datosTemp = calculaEscaños(circunscripciones[i], candidaturas, votosCircunscripciones[i], propMinRepresentacion);
+        
+        // Añadimos los escaños de la circunscripcion a su respectiva candidatura
+        for (var j = 0; j < datos.length; j++) {
+            for (var k = 0; k < datosTemp.length; k++) {
+                if (datos[j].candidatura === datosTemp[k].candidatura) {
+                    datos[j].numeroEscaños += datosTemp[k].numeroEscaños;
+                    k = datos.length;
+                }
+            }
+        }
+    }
+    
+    datos = ordenaPorEscaños(datos);
+    
+    // Dibujamos el gráfico grande
     var contenedor = document.getElementById("charts2");
     while(contenedor.hasChildNodes()){
         contenedor.removeChild(contenedor.firstChild);
     }
     
-    var datos = [];
-    var numRepresentantes = 0;
-    
-    for(var i in circunscripciones){
-        numRepresentantes += circunscripciones[i].numeroRepresentantes;
-    }
-    
-    for(var i = 0 ; i <= indice ; i++){
-        var datosTemp = calculaEscaños(circunscripciones[i], candidaturas, votosCircunscripciones[i], propMinRepresentacion);
-        
-        if (i === 0) {
-            datos = datosTemp;
-        } else {
-            for(var j = 1 ; j <= datosTemp.length ; j++){
-                datos[j].numeroEscaños += datosTemp[j].numeroEscaños;
-            }
-        }
-    }
-    
-    var svg = d3.select("#charts2").append("svg")
-        .attr("width", ancho)
-        .attr("height", alto);
-    var groupArcos = svg.append("g")
-        .attr("transform", "translate(" + ancho/2 + ", " + alto/2 + ")");
+    var divGrande = d3.select("#charts2");
+    var dimensionesDivGrande = divGrande.node().getBoundingClientRect(),
+        centro = {x: dimensionesDivGrande.width/2, y: dimensionesDivGrande.width/2},
+        radioExterior = dimensionesDivGrande.width / 2,
+        radioInterior = radioExterior - 50;
 
-    ultimoAngulo = -Math.PI/2;
+    var svg = divGrande.append("svg")
+        .attr("width", dimensionesDivGrande.width)
+        .attr("height", dimensionesDivGrande.width/2);
+
+    var groupArcos = svg.append("g")
+        .attr("transform", "translate(" + centro.x + ", " + centro.y + ")");
+
+    var ultimoAngulo = -Math.PI/2;
     for (var j in datos) {
         var anguloFinal = ultimoAngulo + datos[j].numeroEscaños * Math.PI / numRepresentantes;
 
